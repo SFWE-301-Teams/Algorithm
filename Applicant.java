0667732d001513bb7e639d743037bb02343d4b0f
aroig1 <aroig@arizona.edu>
2023-11-16 16:29:12 -0700

    variables for Applicant and Scholarship classes

---
+++ Applicant.java
@@ -0,0 +1,19 @@
+public class Applicant {
+    private String name;
+    private Boolean citizenship;
+    private String gender;
+    private String statement;
+    private int studentID;
+    private String email;
+    private String[] interests;
+    
+    private double GPA;
+    private String major;
+    private String minor;
+    private Boolean transfer;
+    private String expGradDate;
+    private String academicYear;
+    private int enrolledUnits;
+
+    
+}
---
+++ Scholarship.java
@@ -0,0 +1,14 @@
+public class Scholarship {
+    private String name;
+    private String[] qualities;
+    private double GPA;
+    private  String major;
+    private String minor;
+    private Boolean transfer;
+    private String expGradDate;
+    private String academicYear;
+    private int enrolledUnits;
+    private String gender;
+    private Boolean citizenship;
+
+}
