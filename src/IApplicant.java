3297cd51f2532db7908679e13f9a70051dccbed3
jath03 <jackthughes@yahoo.com>
2023-11-16 17:16:58 -0700

    Added matching engine skeleton

---
+++ src/IApplicant.java
@@ -0,0 +1,3 @@
+package Algorithm;
+
+interface IApplicant {}
---
+++ src/MatchingEngine.java
@@ -0,0 +1,15 @@
+package Algorithm;
+
+import java.util.ArrayList;
+
+public class MatchingEngine {
+    /**
+    * Returns a list of scholarship that match the provided user
+    *
+    * @param user           The user to match scholarships with
+    * @param scholarships   All of the possible active scholarships that the user could match with
+    */
+    public static ArrayList<Class<? extends IScholarship>> match(Class<? extends IApplicant> user, ArrayList<Class<? extends IScholarship>> scholarships) {
+        return null;
+    }
+}
