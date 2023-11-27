f807bc7f514b1e429d1cf928e5cc8dc1caf5c038
Jack <jackthughes@yahoo.com>
2023-11-26 17:02:02 -0700

    Added applicant validation (#4)
    * Added applicant validation
    
    * Fix build error

--- lib/src/main/java/algorithm/MatchingEngine.java
+++ lib/src/main/java/algorithm/MatchingEngine.java
@@ -1,6 +1,7 @@
 package algorithm;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class MatchingEngine {
     /**
@@ -9,7 +10,28 @@ public class MatchingEngine {
      * @param user           The user to match scholarships with
      * @param scholarships   All of the possible active scholarships that the user could match with
      */
-    public static ArrayList<IScholarship> match(IApplicant user, ArrayList<IScholarship> scholarships) {
+    public static ArrayList<IScholarship> match(IApplicant applicant, ArrayList<IScholarship> scholarships) {
         return null;
+    }
+
+    /**
+     * Checks whether all of the fields of an applicant contain valid data
+     * @param  applicant  The applicant to check
+     * @return           True if applicant is valid
+     */
+    static boolean validateApplicant(IApplicant applicant) {
+        return (
+            (Objects.nonNull(applicant.getUSCitizenship())) &&
+            (Objects.nonNull(applicant.getGPA())) &&
+            (Objects.nonNull(applicant.getTransfer())) &&
+            (Objects.nonNull(applicant.getStatement())) &&
+            (Objects.nonNull(applicant.getMinors())) &&
+            (Objects.nonNull(applicant.getMajors())) &&
+            (Objects.nonNull(applicant.getInterests())) &&
+            (Objects.nonNull(applicant.getExpGradDate())) &&
+            (Objects.nonNull(applicant.getEnrolledUnits())) &&
+            (Objects.nonNull(applicant.getGender())) &&
+            (Objects.nonNull(applicant.getAcademicYear()))
+        );
     }
 }
